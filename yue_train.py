6c30a9c5c50a2e90da07e9a513e7e2afc4da4a80
Yue Liu <150256922+chdliuyue@users.noreply.github.com>
2025-12-16 14:06:04 +0800

    Switch model DUET

--- yue_train.py
+++ yue_train.py
@@ -1,11 +1,12 @@
 from basicts import BasicTSLauncher
 from basicts.configs import BasicTSForecastingConfig
-from basicts.models.GDGCN import GDGCN, GDGCNConfig
+from basicts.models.DUET import DUET, DUETConfig
+from basicts.runners.callback import AddAuxiliaryLoss
 
 
 def main():
 
-    model_config = GDGCNConfig(
+    model_config = DUETConfig(
         input_len=12,
         output_len=12,
         num_features=358,
@@ -13,13 +14,14 @@ def main():
 
 
     BasicTSLauncher.launch_training(BasicTSForecastingConfig(
-        model=GDGCN,
+        model=DUET,
         input_len=12,
         output_len=12,
         model_config=model_config,
         dataset_name="PEMS03",
         metrics=["MAE", "MSE", "RMSE", "MAPE", "WAPE", "SMAPE", "R2", "CORR", "HUBER"],
         loss="MAE",
+        callbacks=[AddAuxiliaryLoss(["aux_loss"])],
         gpus="0"
     ))
 
